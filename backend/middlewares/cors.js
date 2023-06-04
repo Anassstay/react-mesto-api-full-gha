9718f4e4e3a0adc95f2910c9fb033ec2696f74c5
A.nassstay <Anastasia.stay@yandex.ru>
2023-06-04 21:49:32 +0300

    as

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -14,11 +14,12 @@ module.exports = (req, res, next) => {
   // сохраняем список заголовков исходного запроса
   const requestHeaders = req.headers['access-control-request-headers'];
 
+  res.header('Access-Control-Allow-Credentials', true);
+
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
 
   // Если это предварительный запрос, добавляем нужные заголовки
